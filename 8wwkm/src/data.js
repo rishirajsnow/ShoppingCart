1215b4d0fc177183e27e8880cc6619b86ef6f757
rishirajsnow <56679468+rishirajsnow@users.noreply.github.com>
2021-11-30 01:48:22 +0530

    Add files via upload

---
+++ 8wwkm/src/data.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const data = {
+  products: [
+    { id: "1", name: "cereal", price: 100 },
+    { id: "2", name: "carrot", price: 200 },
+    { id: "3", name: "cucumber", price: 300 }
+  ]
+};
+
+export default data;
